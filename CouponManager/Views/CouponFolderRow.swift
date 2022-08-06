6e84870d34003ab8df4b24e60fa049c6e64f4748
hyperbora <hyperbora@gmail.com>
2022-08-06 10:10:09 +0900

    comment at preview code

--- CouponManager/Views/CouponFolderRow.swift
+++ CouponManager/Views/CouponFolderRow.swift
@@ -22,24 +22,9 @@ struct CouponFolderRow: View {
 }
 
 //struct CouponFolderRow_Previews: PreviewProvider {
-//    static var store = getStore()
-//    static func getStore() -> FolderStore {
-//        let store = FolderStore()
-//        store.addFolder(folderName: "Amazon")
-//        store.addFolder(folderName: "Ebay")
-//        return store
-//    }
-//
 //    static var previews: some View {
-//
 //        Group {
-//            if let name = store.folders.first?.folderName, let imageName = store.folders.first?.folderType.description, let count =
-//                store.folders.first?.count {
-//                CouponFolderRow(name: name, folderImageName: imageName, count: String(count))
-//            }
-//            if let name = store.folders.last?.folderName, let imageName = store.folders.last?.folderType.description, let count = store.folders.last?.count {
-//                CouponFolderRow(name: name, folderImageName: imageName, count: String(count))
-//            }
+//            CouponFolderRow(folder: Folder())
 //        }.previewLayout(.fixed(width: 300, height: 70))
 //    }
 //}
